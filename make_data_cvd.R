e2bfcdeafe60ffeb6d0e4c3d0ab35ebc07b14f28
Madison Coots <madisoncoots@Madisons-MacBook-Pro.local>
2023-09-14 14:03:58 -0400

    first stab as of 9/14

---
+++ make_data_cvd.R
@@ -0,0 +1,437 @@
+# Author: Madison Coots
+# Date: September 13, 2023
+# ----------------------
+# This file pulls all of the requisite NHANES data tables (from 2011-2018)
+# and performs a join for the analysis. The following tables are downloaded:
+# 1. Demographics (DEMO)
+# 2. Diabetes questionnaire responses (DIQ)
+# 3. Blood pressure and cholesterol (BPQ)
+# 4. Body measurements (BMX)
+# 5. Glycohemoglobin measurements (GHB)
+# 6. Health insurance status (HIQ)
+# 7. Physical activity data (PAQ)
+# 8. Medical conditions data (MCQ)
+# 9. Mental health data (DPQ)
+# 10. Smoking history (SMQ)
+# 11. Total cholesterol (TCHOL)
+# 12. High-density lipoprotein (HDL)
+# 13. Low-density lipoprotein (TRIGLY)
+
+#
+# After joining the tables across years, this script implements the same
+# filtering on age, BMI, and pregnancy status described in Aggarwal et al. (2022).
+# We also replicate this paper's method of constructing the diabetes labels.
+#
+# The full data object is written to the path specified in write_path.
+
+library(haven)
+library(tidyverse)
+library(janitor)
+
+data_object_write_path <- here::here(dirname(rstudioapi::getActiveDocumentContext()$path), "data/processed/")
+table_write_path <- here::here(dirname(rstudioapi::getActiveDocumentContext()$path), "data/raw/")
+
+# 2011-2012
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/DEMO_G.XPT", demo <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/DIQ_G.XPT", diq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/BPQ_G.XPT", bpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/BMX_G.XPT", bmx <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/GHB_G.XPT", ghb <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/HIQ_G.XPT", hiq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/PAQ_G.XPT", paq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/MCQ_G.XPT", mcq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/DPQ_G.XPT", dpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/SMQ_G.XPT", smq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/TCHOL_G.XPT", tchol <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/HDL_G.XPT", hdl <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2011-2012/TRIGLY_G.XPT", trigly <- tempfile(), mode="wb")
+
+raw_demographics_11_12 <- foreign::read.xport(demo) %>% 
+  clean_names()
+raw_survey_responses_11_12 <- foreign::read.xport(diq) %>% 
+  clean_names()
+raw_bp_and_chol_11_12 <- foreign::read.xport(bpq) %>% 
+  clean_names()
+raw_body_measurements_11_12 <- foreign::read.xport(bmx) %>% 
+  clean_names()
+raw_glycohemoglobin_11_12 <- foreign::read.xport(ghb) %>% 
+  clean_names()
+raw_health_ins_11_12 <- foreign::read.xport(hiq) %>% 
+  clean_names()
+raw_physical_act_11_12 <- foreign::read.xport(paq) %>% 
+  clean_names()
+raw_medical_cond_11_12 <- foreign::read.xport(mcq) %>% 
+  clean_names()
+raw_mental_health_11_12 <- foreign::read.xport(dpq) %>% 
+  clean_names()
+raw_smoking_11_12 <- foreign::read.xport(smq) %>% 
+  clean_names()
+raw_tchol_11_12 <- foreign::read.xport(tchol) %>% 
+  clean_names()
+raw_hdl_11_12 <- foreign::read.xport(hdl) %>% 
+  clean_names()
+raw_ldl_11_12 <- foreign::read.xport(trigly) %>% 
+  clean_names()
+
+
+
+# 2013-2014
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/DEMO_H.XPT", demo <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/DIQ_H.XPT", diq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/BPQ_H.XPT", bpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/BMX_H.XPT", bmx <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/GHB_H.XPT", ghb <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/HIQ_H.XPT", hiq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/PAQ_H.XPT", paq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/MCQ_H.XPT", mcq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/DPQ_H.XPT", dpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/SMQ_H.XPT", smq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/TCHOL_H.XPT", tchol <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/HDL_H.XPT", hdl <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2013-2014/TRIGLY_H.XPT", trigly <- tempfile(), mode="wb")
+
+raw_demographics_13_14 <- foreign::read.xport(demo) %>% 
+  clean_names()
+raw_survey_responses_13_14 <- foreign::read.xport(diq) %>% 
+  clean_names()
+raw_bp_and_chol_13_14 <- foreign::read.xport(bpq) %>% 
+  clean_names()
+raw_body_measurements_13_14 <- foreign::read.xport(bmx) %>% 
+  clean_names()
+raw_glycohemoglobin_13_14 <- foreign::read.xport(ghb) %>% 
+  clean_names()
+raw_health_ins_13_14 <- foreign::read.xport(hiq) %>% 
+  clean_names()
+raw_physical_act_13_14 <- foreign::read.xport(paq) %>% 
+  clean_names()
+raw_medical_cond_13_14 <- foreign::read.xport(mcq) %>% 
+  clean_names()
+raw_mental_health_13_14 <- foreign::read.xport(dpq) %>% 
+  clean_names()
+raw_smoking_13_14 <- foreign::read.xport(smq) %>% 
+  clean_names()
+raw_tchol_13_14 <- foreign::read.xport(tchol) %>% 
+  clean_names()
+raw_hdl_13_14 <- foreign::read.xport(hdl) %>% 
+  clean_names()
+raw_ldl_13_14 <- foreign::read.xport(trigly) %>% 
+  clean_names()
+
+
+
+# 2015-2016
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/DEMO_I.XPT", demo <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/DIQ_I.XPT", diq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/BPQ_I.XPT", bpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/BMX_I.XPT", bmx <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/GHB_I.XPT", ghb <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/HIQ_I.XPT", hiq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/PAQ_I.XPT", paq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/MCQ_I.XPT", mcq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/DPQ_I.XPT", dpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/SMQ_I.XPT", smq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/TCHOL_I.XPT", tchol <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/HDL_I.XPT", hdl <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2015-2016/TRIGLY_I.XPT", trigly <- tempfile(), mode="wb")
+
+raw_demographics_15_16 <- foreign::read.xport(demo) %>% 
+  clean_names()
+raw_survey_responses_15_16 <- foreign::read.xport(diq) %>% 
+  clean_names()
+raw_bp_and_chol_15_16 <- foreign::read.xport(bpq) %>% 
+  clean_names()
+raw_body_measurements_15_16 <- foreign::read.xport(bmx) %>% 
+  clean_names()
+raw_glycohemoglobin_15_16 <- foreign::read.xport(ghb) %>% 
+  clean_names()
+raw_health_ins_15_16 <- foreign::read.xport(hiq) %>% 
+  clean_names()
+raw_physical_act_15_16 <- foreign::read.xport(paq) %>% 
+  clean_names()
+raw_medical_cond_15_16 <- foreign::read.xport(mcq) %>% 
+  clean_names()
+raw_mental_health_15_16 <- foreign::read.xport(dpq) %>% 
+  clean_names()
+raw_smoking_15_16 <- foreign::read.xport(smq) %>% 
+  clean_names()
+raw_tchol_15_16 <- foreign::read.xport(tchol) %>% 
+  clean_names()
+raw_hdl_15_16 <- foreign::read.xport(hdl) %>% 
+  clean_names()
+raw_ldl_15_16 <- foreign::read.xport(trigly) %>% 
+  clean_names()
+
+
+# 2017-2018
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/DEMO_J.XPT", demo <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/DIQ_J.XPT", diq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/BPQ_J.XPT", bpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/BMX_J.XPT", bmx <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/GHB_J.XPT", ghb <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/HIQ_J.XPT", hiq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/PAQ_J.XPT", paq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/MCQ_J.XPT", mcq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/DPQ_J.XPT", dpq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/SMQ_J.XPT", smq <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/TCHOL_J.XPT", tchol <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/HDL_J.XPT", hdl <- tempfile(), mode="wb")
+download.file("https://wwwn.cdc.gov/nchs/nhanes/2017-2018/TRIGLY_J.XPT", trigly <- tempfile(), mode="wb")
+
+raw_demographics_17_18 <- foreign::read.xport(demo) %>% 
+  clean_names()
+raw_survey_responses_17_18 <- foreign::read.xport(diq) %>% 
+  clean_names()
+raw_bp_and_chol_17_18 <- foreign::read.xport(bpq) %>% 
+  clean_names()
+raw_body_measurements_17_18 <- foreign::read.xport(bmx) %>% 
+  clean_names()
+raw_glycohemoglobin_17_18 <- foreign::read.xport(ghb) %>% 
+  clean_names()
+raw_health_ins_17_18 <- foreign::read.xport(hiq) %>% 
+  clean_names()
+raw_physical_act_17_18 <- foreign::read.xport(paq) %>% 
+  clean_names()
+raw_medical_cond_17_18 <- foreign::read.xport(mcq) %>% 
+  clean_names()
+raw_mental_health_17_18 <- foreign::read.xport(dpq) %>% 
+  clean_names()
+raw_smoking_17_18 <- foreign::read.xport(smq) %>% 
+  clean_names()
+raw_tchol_17_18 <- foreign::read.xport(tchol) %>% 
+  clean_names()
+raw_hdl_17_18 <- foreign::read.xport(hdl) %>% 
+  clean_names()
+raw_ldl_17_18 <- foreign::read.xport(trigly) %>% 
+  clean_names()
+
+
+# ------------------- Combine -------------------
+
+# Demographics data
+raw_demographics_all <- bind_rows(
+  raw_demographics_11_12,
+  raw_demographics_13_14,
+  raw_demographics_15_16,
+  raw_demographics_17_18
+)
+
+# Diabetes survey data
+raw_survey_responses_all <- bind_rows(
+  raw_survey_responses_11_12,
+  raw_survey_responses_13_14,
+  raw_survey_responses_15_16,
+  raw_survey_responses_17_18
+)
+
+# Blood pressure and cholesterol data
+raw_bp_and_chol_all <- bind_rows(
+  raw_bp_and_chol_11_12,
+  raw_bp_and_chol_13_14,
+  raw_bp_and_chol_15_16,
+  raw_bp_and_chol_17_18
+)
+
+# Body measurements data
+raw_body_measurements_all <- bind_rows(
+  raw_body_measurements_11_12,
+  raw_body_measurements_13_14,
+  raw_body_measurements_15_16,
+  raw_body_measurements_17_18
+)
+
+# Glycohemoglobin data
+raw_glycohemoglobin_all <- bind_rows(
+  raw_glycohemoglobin_11_12,
+  raw_glycohemoglobin_13_14,
+  raw_glycohemoglobin_15_16,
+  raw_glycohemoglobin_17_18
+)
+
+# Health insurance data
+raw_health_ins_all <- bind_rows(
+  raw_health_ins_11_12,
+  raw_health_ins_13_14,
+  raw_health_ins_15_16,
+  raw_health_ins_17_18
+)
+
+# Physical activity data
+raw_physical_act_all <- bind_rows(
+  raw_physical_act_11_12,
+  raw_physical_act_13_14,
+  raw_physical_act_15_16,
+  raw_physical_act_17_18
+)
+
+# Medical conditions data
+raw_medical_cond_all <- bind_rows(
+  raw_medical_cond_11_12,
+  raw_medical_cond_13_14,
+  raw_medical_cond_15_16,
+  raw_medical_cond_17_18
+)
+
+# Mental health data
+raw_mental_health_all <- bind_rows(
+  raw_mental_health_11_12,
+  raw_mental_health_13_14,
+  raw_mental_health_15_16,
+  raw_mental_health_17_18
+)
+
+# Smoking history data
+raw_smoking_all <- bind_rows(
+  raw_smoking_11_12,
+  raw_smoking_13_14,
+  raw_smoking_15_16,
+  raw_smoking_17_18
+)
+
+# Total cholesterol data
+raw_tchol_all <- bind_rows(
+  raw_tchol_11_12,
+  raw_tchol_13_14,
+  raw_tchol_15_16,
+  raw_tchol_17_18
+)
+
+# HDL data
+raw_hdl_all <- bind_rows(
+  raw_hdl_11_12,
+  raw_hdl_13_14,
+  raw_hdl_15_16,
+  raw_hdl_17_18
+)
+
+# LDL data
+raw_ldl_all <- bind_rows(
+  raw_ldl_11_12,
+  raw_ldl_13_14,
+  raw_ldl_15_16,
+  raw_ldl_17_18
+)
+
+write_csv(raw_demographics_all, file = paste(table_write_path, "raw_demographics_all.csv", sep = ""))
+write_csv(raw_survey_responses_all, file = paste(table_write_path, "raw_survey_responses_all.csv", sep = ""))
+write_csv(raw_bp_and_chol_all, file = paste(table_write_path, "raw_bp_and_chol_all.csv", sep = ""))
+write_csv(raw_body_measurements_all, file = paste(table_write_path, "raw_body_measurements_all.csv", sep = ""))
+write_csv(raw_glycohemoglobin_all, file = paste(table_write_path, "raw_glycohemoglobin_all.csv", sep = ""))
+write_csv(raw_health_ins_all, file = paste(table_write_path, "raw_health_ins_all.csv", sep = ""))
+write_csv(raw_physical_act_all, file = paste(table_write_path, "raw_physical_act_all.csv", sep = ""))
+write_csv(raw_medical_cond_all, file = paste(table_write_path, "raw_medical_cond_all.csv", sep = ""))
+write_csv(raw_mental_health_all, file = paste(table_write_path, "raw_mental_health_all.csv", sep = ""))
+write_csv(raw_smoking_all, file = paste(table_write_path, "raw_smoking_all.csv", sep = ""))
+write_csv(raw_tchol_all, file = paste(table_write_path, "raw_tchol_all.csv", sep = ""))
+write_csv(raw_hdl_all, file = paste(table_write_path, "raw_hdl_all.csv", sep = ""))
+write_csv(raw_ldl_all, file = paste(table_write_path, "raw_ldl_all.csv", sep = ""))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+data <- raw_demographics_all %>%
+  inner_join(raw_survey_responses_all, by = c("seqn")) %>%
+  inner_join(raw_bp_and_chol_all, by = c("seqn")) %>%
+  inner_join(raw_body_measurements_all, by = c("seqn")) %>%
+  inner_join(raw_glycohemoglobin_all, by = c("seqn")) %>%
+  inner_join(raw_health_ins_all, by = c("seqn")) %>%
+  inner_join(raw_physical_act_all, by = c("seqn")) %>%
+  inner_join(raw_medical_cond_all, by = c("seqn")) %>%
+  inner_join(raw_mental_health_all, by = c("seqn")) %>%
+  inner_join(raw_smoking_all, by = c("seqn")) %>%
+  # inner_join(raw_tchol_all, by = c("seqn")) %>%
+  # inner_join(raw_hdl_all, by = c("seqn")) %>%
+  # inner_join(raw_ldl_all, by = c("seqn")) %>%
+  filter(ridageyr >= 18) %>% # Taken from Supplement
+  filter(ridageyr <= 70) %>%
+  filter(ridexprg != 1 | is.na(ridexprg)) %>%
+  filter(bmxbmi >= 18.5, # Taken from Supplement
+         bmxbmi <= 50) %>%
+  # For this condition, we allow NA responses because 
+  # an NA response indicates that the patient was never 
+  # told to take statins and therefore weren't asked
+  # whether they were currently using statins, hence the NA.
+  # A value of 2 indicates a negative response even though
+  # they were told to take statins 
+  filter(is.na(bpq100d) | (bpq100d == 2)) %>%
+  # Making the race variable more readable
+  mutate(
+    race = case_when(ridreth3 == 1 | ridreth3 == 2 ~ "Hispanic",
+                     ridreth3 == 3 ~ "White",
+                     ridreth3 == 4 ~ "Black",
+                     ridreth3 == 6 ~ "Asian"),
+    race = factor(race),
+    # Re-leveling the race factor, so that White is base level (as in paper)
+    race = relevel(race, ref = "White")) %>%
+  # Recoding the gender covariate
+  mutate(gender = case_when(riagendr == 2 ~ "Woman",
+                            riagendr == 1 ~ "Man",
+                            riagendr == "." ~ as.character(NA))) %>%
+  # Recoding the income codes to interpretable values
+  mutate(income = case_when(indhhin2 == 1 ~ "$0 to $4,999",
+                            indhhin2 == 2 ~ "$5,000 to $9,999",
+                            indhhin2 == 3 ~ "$10,000 to $14,999",
+                            indhhin2 == 4 ~ "$15,000 to $19,999",
+                            indhhin2 == 5 ~ "$20,000 to $24,999",
+                            indhhin2 == 6 ~ "$25,000 to $34,999",
+                            indhhin2 == 7 ~ "$35,000 to $44,999",
+                            indhhin2 == 8 ~ "$45,000 to $54,999",
+                            indhhin2 == 9 ~ "$55,000 to $64,999",
+                            indhhin2 == 10 ~ "$65,000 to $74,999",
+                            indhhin2 == 12 ~ "$20,000 and Over",
+                            indhhin2 == 13 ~ "Under $20,000",
+                            indhhin2 == 14 ~ "$75,000 to $99,999",
+                            indhhin2 == 15 ~ "$100,000 and Over",
+                            indhhin2 %in% c(77,99,".") ~ as.character(NA))
+  ) %>%
+  # Recoding the health insurance codes to interpretable values
+  mutate(health_insurance = case_when(hiq011 == 1 ~ "Yes",
+                                      hiq011 == 2 ~ "No",
+                                      hiq011 %in% c(7, 9, ".") ~ as.character(NA))
+  ) %>%
+  # Recoding the felt depressed covariate
+  mutate(felt_depressed = case_when(dpq020 == 0 ~ "Not at all",
+                                    dpq020 == 1 ~ "Several days",
+                                    dpq020 == 2 ~ "More than half the days",
+                                    dpq020 == 3 ~ "Nearly every day",
+                                    dpq020 %in% c(7, 9, ".") ~ as.character(NA))) %>%
+  # Adjusting weights for 8 years of data
+  mutate(wtmec8yr = wtmec2yr/4) %>%
+  # Making diabetes labels as described in the paper and replication code
+  mutate(lbxgh = as.numeric(as.character((lbxgh))),
+         diq010 = as.numeric(as.character((diq010))),
+         a1c = cut(lbxgh,breaks=c(0,5.7,6.5,1000),right=FALSE),
+         diabetes_diagnosis = case_when(diq010 %in% 1 ~ 1,
+                                        diq010 %in% c(2,3,9) ~ 0,
+                                        diq010 %in% 7 ~ as.numeric(NA)),
+         diabetes = diabetes_diagnosis,
+         diabetes = if_else(a1c=="[6.5,1e+03)" &!is.na(a1c), 1, diabetes),
+         diabetes = as.integer(diabetes),
+         diabetes = if_else(diabetes == 1, TRUE, FALSE)
+  ) %>%
+  # Constructing the CVD label
+  mutate(congestive_heart_failure = (mcq160b == 1),
+         coronary_heart_disease = (mcq160c == 1),
+         angina = (mcq160d == 1),
+         heart_attack = (mcq160e == 1),
+         stroke = (mcq160f == 1),
+         cvd = (congestive_heart_failure | coronary_heart_disease | angina | heart_attack | stroke)) %>%
+  drop_na(cvd) %>%
+  # Extracting smoking label (smokes everyday or sometimes)
+  mutate(smokes = (smq040 == 1) | (smq040 == 2)) %>%
+  # Normalizing weights for numerical stability in regressions
+  mutate(normalized_weights = wtmec8yr / sum(wtmec8yr))
+
+saveRDS(data, file = paste(data_object_write_path, "cvd_data.rds", sep = ""))
