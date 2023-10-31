31e2a5b1c8ff11acb0af39c770c3d3d0595bb1fd
Madison Coots <madisoncoots@Madisons-MacBook-Pro.local>
2023-10-31 17:43:31 -0400

    first pass at breast cancer analysis

---
+++ bc/bc_working_analysis_script.R
@@ -0,0 +1,143 @@
+# Author: Madison Coots
+# Date: October 31, 2023
+# ======================
+# Code for all figures, tables, and statistics 
+# in Coots et al. (2023)
+
+library(tidyverse)
+library(readr)
+library(janitor)
+library(stats)
+
+directory_path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+save_path <- here::here(directory_path, 'figures/')
+
+source(here::here(directory_path, 'colors.R'))
+
+data <- readRDS(here::here(directory_path, 'data/processed', 'all_data_with_bc_risk.rds')) %>%
+  rename(race = race_str)
+
+theme_set(theme_bw(base_size = 15))
+
+# ===========================================================================================
+# ======================================= Figures ===========================================
+
+risk_score_upper_bound <- 0.025
+incidence_upper_bound <- 0.025
+screening_thresh <- 0.0166
+# 
+# ===========================================================================================
+# ============================== Race-aware calibration plot ================================
+# ===========================================================================================
+
+data %>%
+  select(race, race_aware_risk, wtmec8yr) %>%
+  mutate(bin = floor((race_aware_risk  + 0.0025) * 100 * 2) / 2 / 100,
+         bucket = ntile(race_aware_risk / 0.025, 10)) %>%
+  group_by(race, bin) %>%
+  summarize(n_in_bin = sum(wtmec8yr),
+            bin_avg = sum(race_aware_risk * wtmec8yr) / sum(wtmec8yr),
+            cvd_prev = sum(race_aware_risk * wtmec8yr) / sum(wtmec8yr)) %>%
+  ggplot(aes(x=bin_avg, y=cvd_prev, color=race)) +
+  geom_vline(xintercept=screening_thresh) +
+  geom_line() + 
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "darkgray") +
+  xlab("Race-aware BC risk") +
+  ylab("True BC Risk") +
+  scale_y_continuous(labels = scales::percent,
+                     breaks = seq(0.0, incidence_upper_bound, 0.005)) +
+  scale_x_continuous(labels = scales::percent,
+                     breaks = seq(0.0, risk_score_upper_bound, 0.005)) +
+  coord_cartesian(xlim = c(0, risk_score_upper_bound), ylim = c(0, incidence_upper_bound)) +
+  theme(legend.title = element_blank(),
+        legend.position = c(0.18, 0.81)) + 
+  scale_color_manual(values=group_color_map,
+                     breaks =group_names)
+
+# ===========================================================================================
+# ============================== Race-blind calibration plot ================================
+# ===========================================================================================
+
+data %>%
+  select(race, race_blind_risk, race_aware_risk, wtmec8yr) %>%
+  mutate(bin = floor((race_blind_risk  + 0.0025) * 100 * 2) / 2 / 100,
+         bucket = ntile(race_blind_risk / 0.025, 10)) %>%
+  group_by(race, bin) %>%
+  summarize(n_in_bin = sum(wtmec8yr),
+            bin_avg = sum(race_blind_risk * wtmec8yr) / sum(wtmec8yr),
+            cvd_prev = sum(race_aware_risk * wtmec8yr) / sum(wtmec8yr)) %>%
+  ggplot(aes(x=bin_avg, y=cvd_prev, color=race)) +
+  geom_vline(xintercept=screening_thresh) +
+  geom_line() + 
+  geom_point() +
+  geom_abline(slope = 1, intercept = 0, linetype = "dashed", color = "darkgray") +
+  xlab("Race-blind BC risk") +
+  ylab("True BC Risk") +
+  scale_y_continuous(labels = scales::percent,
+                     breaks = seq(0.0, incidence_upper_bound, 0.005)) +
+  scale_x_continuous(labels = scales::percent,
+                     breaks = seq(0.0, risk_score_upper_bound, 0.005)) +
+  coord_cartesian(xlim = c(0, risk_score_upper_bound), ylim = c(0, incidence_upper_bound)) +
+  theme(legend.title = element_blank(),
+        legend.position = c(0.18, 0.81)) + 
+  scale_color_manual(values=group_color_map,
+                     breaks =group_names)
+  
+# ===========================================================================================
+# ============================== Race-blind risk distribution ===============================
+# ===========================================================================================
+
+data %>%
+  select(race, race_blind_risk, race_aware_risk, wtmec8yr) %>%
+  ggplot(aes(x = race_blind_risk, weight = wtmec8yr, fill = race)) +
+  facet_wrap(vars(fct_rev(race)), ncol = 2) +
+  geom_histogram(binwidth = 0.001, aes(y = after_stat(count/tapply(count, PANEL, sum)[PANEL]))) +
+  geom_vline(xintercept=screening_thresh) + 
+  scale_fill_manual(values=group_color_map,
+                    breaks =group_names) +
+  scale_y_continuous(labels = scales::percent,
+                     breaks = seq(0.0, 1, 0.02)) +
+  scale_x_continuous(labels = scales::percent) +
+  coord_cartesian(xlim = c(0, 0.03), ylim = c(0, 0.18)) +
+  xlab("Race-blind BC risk") +
+  ylab("Population proportion (%)") + 
+  theme(legend.title = element_blank(),
+        legend.position = c(0.85, 0.86))
+
+# ===========================================================================================
+# ============================== Race-aware risk distribution ===============================
+# ===========================================================================================
+
+data %>%
+  select(race, race_blind_risk, race_aware_risk, wtmec8yr) %>%
+  ggplot(aes(x = race_aware_risk, weight = wtmec8yr, fill = race)) +
+  facet_wrap(vars(fct_rev(race)), ncol = 2) +
+  geom_histogram(binwidth = 0.001, aes(y = after_stat(count/tapply(count, PANEL, sum)[PANEL]))) +
+  geom_vline(xintercept=screening_thresh) + 
+  scale_fill_manual(values=group_color_map,
+                    breaks =group_names) +
+  scale_y_continuous(labels = scales::percent,
+                     breaks = seq(0.0, 1, 0.02)) +
+  scale_x_continuous(labels = scales::percent) +
+  coord_cartesian(xlim = c(0, 0.03), ylim = c(0, 0.18)) +
+  xlab("Race-aware BC risk") +
+  ylab("Population proportion (%)") + 
+  theme(legend.title = element_blank(),
+        legend.position = c(0.85, 0.86))
+  
+# ===========================================================================================
+# ==================================== Statistics ===========================================
+# ====== Pct. of people with the same screening decision under both models =====
+data %>%
+  select(ID, race, race_blind_risk, race_aware_risk, wtmec8yr) %>%
+  mutate(race_blind_screening_decision = race_blind_risk > screening_thresh,
+         race_aware_screening_decision = race_aware_risk > screening_thresh,
+         same_decision = (race_blind_screening_decision == race_aware_screening_decision)) %>%
+  group_by(race) %>%
+  summarize(pct_blind_decision = sum(race_blind_screening_decision * wtmec8yr) / sum(wtmec8yr) * 100,
+            pct_aware_decision = sum(race_aware_screening_decision * wtmec8yr) / sum(wtmec8yr) * 100,
+            pct_same_decision = sum(same_decision * wtmec8yr) / sum(wtmec8yr) * 100) %>%
+  arrange(pct_same_decision) %>%
+  select(race, pct_same_decision)
+
--- bc/gail_equation.R
+++ bc/gail_equation.R
@@ -1,15 +1,67 @@
+# Author: Madison Coots
+# Date: October 31, 2023
+# ----------------------
+
+library(readr)
+library(janitor)
 library(BCRA)
 library(tidyverse)
 
-View(absolute.risk)
+directory_path <- dirname(rstudioapi::getActiveDocumentContext()$path)
 
-data("exampledata")
-absolute.risk(exampledata %>% select(-T2))
+data <- readRDS(here::here(directory_path, 'data/processed', 'bc_data.rds'))
 
-data("BrCa_1_AR")
-BrCa_1_AR
+readRDS("Downloads/all_data_with_ascvd.rds") %>% View()
 
-data("BrCa_beta")
-BrCa_beta
+# ======================================================================
+# Predicting race-aware 5-year breast cancer risk (standard)
+# ======================================================================
 
-error.table(data, Raw_Ind=1)
+all_data_with_race_aware_risk <- data %>%
+  mutate(race_aware_risk = absolute.risk(data))
+
+# ======================================================================
+# Predicting race-blind 5-year breast cancer risk (non-standard)
+# ======================================================================
+
+race_breakdown <- data %>%
+  group_by(Race) %>%
+  summarize(n = sum(wtmec8yr)) %>%
+  ungroup() %>%
+  mutate(prop = n / sum(n))
+
+prop_white <- race_breakdown %>% filter(Race == 1) %>% pull(prop)
+prop_black <- race_breakdown %>% filter(Race == 2) %>% pull(prop)
+prop_us_hispanic <- race_breakdown %>% filter(Race == 3) %>% pull(prop)
+prop_non_us_hispanic <- race_breakdown %>% filter(Race == 5) %>% pull(prop)
+prop_asian <- race_breakdown %>% filter(Race == 11) %>% pull(prop)
+
+all_data_with_race_blind_risk <- data %>%
+  mutate(white_risk = absolute.risk(data %>% mutate(Race = 1)),
+         black_risk = absolute.risk(data %>% mutate(Race = 2)),
+         us_hispanic_risk = absolute.risk(data %>% mutate(Race = 3)),
+         non_us_hispanic_risk = absolute.risk(data %>% mutate(Race = 5)),
+         asian_risk = absolute.risk(data %>% mutate(Race = 11)),
+         race_blind_risk = (
+           prop_white * white_risk +
+           prop_black * black_risk +
+           prop_us_hispanic * us_hispanic_risk +
+           prop_non_us_hispanic * non_us_hispanic_risk +
+           prop_asian * asian_risk
+         )) %>%
+  select(-white_risk, -black_risk, -us_hispanic_risk, -non_us_hispanic_risk, -asian_risk)
+
+# ======================================================================
+# Saving data and both predictions
+# ======================================================================
+
+all_data_with_both_risk <-
+  all_data_with_race_aware_risk %>%
+  left_join(all_data_with_race_blind_risk %>%
+              select(ID, race_blind_risk), by = c("ID")) %>%
+  mutate(race_aware_risk = race_aware_risk / 100,
+         race_blind_risk = race_blind_risk / 100)
+
+saveRDS(all_data_with_both_risk, file = paste(data_object_write_path, "all_data_with_bc_risk.rds", sep = ""))
+
+saveRDS(all_data_with_both_ascvd, file = paste(data_object_write_path, "all_data_with_ascvd_2013_PCE.rds", sep = ""))  
